72c767f4e42b8a9d6bfea93e7472b3ba706cfe38
okolie emmanuel <Leoemmanuel93@gmail.com>
2025-03-21 02:36:22 +0100

    Security Update

--- Movie-project/frontend/src/components/MovieCard.jsx
+++ Movie-project/frontend/src/components/MovieCard.jsx
@@ -10,7 +10,7 @@ function MovieCard({movie}){
             <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
             <div className="movie-overlay">
                 <button className="favorite-btn" onClick={onFavouriteClick}>
-                ❤️
+                🤍
                 </button>
             </div>
         </div>
