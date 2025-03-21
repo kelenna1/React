708c4264760a33b994ef8e38fcbe1691692de2f5
okolie emmanuel <Leoemmanuel93@gmail.com>
2025-03-21 01:23:20 +0100

    created a movie app that fetches data from an API

---
+++ Movie-project/frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import "./css/App.css"
+// import MovieCard from './components/MovieCard'
+import Home from './pages/Home'
+import Favorite from './pages/Favorites'
+import {Routes, Route} from 'react-router-dom'
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    <div>
+      <NavBar/> 
+    <main className='main-content'>
+      <Routes>
+        <Route path='/' element= {<Home/>}/>
+        <Route path='Favorite' element= {<Favorite/>}/>
+      </Routes>
+    </main>
+    </div>
+  )
+}
+
+export default App
---
+++ Movie-project/frontend/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+import "../css/Favorites.css"
+
+function Favorites(){
+    return <div className="favorites-empty">
+        <h2>No favorite movie</h2>
+        <p>Start adding movies to your favorite and they will appear here</p>
+    </div>
+}
+
+export default Favorites
